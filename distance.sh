7c970c22c31b60f960729cb2203b59fd6ec7b528
eriksdav <david.eriksson@ehalsomyndigheten.se>
2014-09-09 08:49:14 +0200

    distance

--- distance.sh
+++ distance.sh
@@ -1,4 +1,6 @@
 RADIUS=6371;
+PI=3.141592653589793238462643383279;
+d2r=0.017453292519943295;
 
 function squareroot() {
 	local NUMBER="$1"
@@ -12,18 +14,19 @@ function tan() {
 
 function cos() {
 	local NUMBER="$1"
-	echo "c($NUMBER)" | bc -l
+	echo "c($NUMBER)" | bc -l | sed 's_^\._0\._g'
 }
 
 function sin() {
 	local NUMBER="$1"
-	echo "s($NUMBER)" | bc -l
+	echo "s($NUMBER)" | bc -l | sed 's_^\._0\._g'
 }
 
 function toRadian() {
 	local DEGREES="$1"
-	local PI=$(echo "scale=10; 4*a(1)" | bc -l)
-	echo "$DEGREES * $( echo $PI / 180 | bc -l )" | bc -l
+	#local PI=$(echo "scale=10; 4*a(1)" | bc -l)
+	#local d2r=$( echo "" | awk "END { print $PI / 180 }" )
+	echo "$DEGREES * $d2r" | bc -l | sed 's_^\._0\._g'  
 }
 
 function haversineFormula() {
@@ -33,20 +36,19 @@ function haversineFormula() {
 	local LONGITUDE_DELTA="$4";
 
 	local SINUS_LAT=$( sin $( echo $LATITUDE_DELTA / 2 | bc -l ) );
-	local SINUS_LAT_TIMES_TWO=$( echo "$SINUS_LAT * $SINUS_LAT" | bc -l );
+	local SINUS_LAT_TIMES_TWO=$( echo "" | awk "END {print $SINUS_LAT ^ $SINUS_LAT }" );
 
 	local COSINUS=$( cos $LATITUDE_DELTA );
-	local COSINUS_TIMES_TWO=$( echo "$COSINUS * $COSINUS" | bc -l );
+	local COSINUS_TIMES_TWO=$( echo "$COSINUS * $COSINUS" | bc -l | sed 's_^\._0\._g' );
 
-	local SINUS_LNG=$( sin $( echo $LONGITUDE_DELTA / 2 | bc -l ) );
-	local SINUS_LNG_TIMES_TWO=$( echo "$SINUS_LNG * $SINUS_LNG" | bc -l );
+	local SINUS_LNG=$( sin $( echo $LONGITUDE_DELTA / 2 | bc -l | sed 's_^\._0\._g' ) );
+	local SINUS_LNG_TIMES_TWO=$( echo "" | awk "END {print $SINUS_LNG ^ $SINUS_LNG }" );
 
 	local a=$( echo "$SINUS_LAT_TIMES_TWO + $COSINUS_TIMES_TWO * $SINUS_LNG_TIMES_TWO" | bc -l );
 
 	local SQUARE_A=$( squareroot $a );
 	local SQUARE_1_MINUS_A=$( squareroot $( echo "1 - $a" | bc -l ) );
 	local TAN_SQUARES=$( echo - | awk "{print atan2($SQUARE_A, $SQUARE_1_MINUS_A)}" | awk {' printf "%4.10f", $1 '} );
-	#local TAN_SQUARES=$( echo "$TAN_SQUARES_SCI" | awk {' printf "%4.12fn", "$1" '} )
 	local TAN_SQUARES_TIMES_TWO=$( echo "$TAN_SQUARES * $TAN_SQUARES" | bc -l );
 
 	local c=$( echo "$RADIUS * $TAN_SQUARES_TIMES_TWO" | bc -l );
@@ -55,9 +57,6 @@ function haversineFormula() {
 }
 
 function calculateDistanceBetween() {
-	echo ONE: "$1"
-	echo TWO: "$2"
-
 	local LATITUDE_ONE=$( echo "$1" | awk -F':' '{print $1}' );
 	local LONGITUDE_ONE=$( echo "$1" | awk -F':' '{print $2}' );
 
@@ -67,8 +66,8 @@ function calculateDistanceBetween() {
 	local LATITUDE_ONE_RADIUS=$( toRadian $LATITUDE_ONE );
 	local LATITUDE_TWO_RADIUS=$( toRadian $LATITUDE_TWO );
 
-	local LATITUDE_DELTA=$( echo $LATITUDE_TWO - $LATITUDE_ONE | bc -l );
-	local LONGITUDE_DELTA=$( echo $LONGITUDE_TWO - $LONGITUDE_ONE | bc -l );
+	local LATITUDE_DELTA=$( echo "" | awk "END {print $LATITUDE_TWO - $LATITUDE_ONE }" | sed 's_^\._0\._g'  );
+	local LONGITUDE_DELTA=$( echo "" | awk "END {print $LONGITUDE_TWO - $LONGITUDE_ONE }" | sed 's_^\._0\._g'  );
 
 	local LATITUDE_DELTA_RADIUS=$( toRadian $LATITUDE_DELTA );
 	local LONGITUDE_DELTA_RADIUS=$( toRadian $LONGITUDE_DELTA );
