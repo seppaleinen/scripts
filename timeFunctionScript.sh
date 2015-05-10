1eea6f471b2c716929df4f3f3f3aa4a931698369
david eriksson <davideriksson@swedenmail.com>
2015-05-10 15:43:32 +0200

    asd

--- timeFunctionScript.sh
+++ timeFunctionScript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Do command, and echo the time it takes to perform
 function logic(){
 	local cmd=$1
 	local i=$2
@@ -9,6 +10,7 @@ function logic(){
 	echo "The command: \"$cmd\" took $T seconds on the $i:th try";
 }
 
+#Loop through logic 5 times.
 function time_func(){
 	local cmd="$1"
 	for i in {1..5}; do 
@@ -16,4 +18,8 @@ function time_func(){
 	done
 }
 
-time_func "$1"
+if [[ "x$1" == x ]]; then
+	echo "You must enter a valid command enclosed in quotations";
+else
+	time_func "$1"
+fi
