2f0704f08cfb8ed54e37524e584dfa467e63072c
david eriksson <davideriksson@swedenmail.com>
2015-07-04 23:25:08 +0200

    more pretty colors in colorTerminal.sh

--- colorTerminal.sh
+++ colorTerminal.sh
@@ -4,8 +4,16 @@
 if [[ -t 1 ]]; then
 	red=$(tput setaf 1);
 	green=$(tput setaf 2);
+	brown=$(tput setaf 3);
+	purple=$(tput setaf 4);
+	pink=$(tput setaf 5);
+	blue=$(tput setaf 6);
 	reset=$(tput sgr0);
 fi
 
 #color the terminal and reset to normal
+printf '%s[+] Converting files \n' "$brown"
+printf '%s[+] Converting files \n' "$purple"
+printf '%s[+] Converting files \n' "$pink"
+printf '%s[+] Converting files \n' "$blue"
 printf '%s[+] Converting files \n' "$green" "$reset"
