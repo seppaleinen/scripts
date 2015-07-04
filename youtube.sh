375891449a7d1c3c0c960b35352e180f6dfa3831
david eriksson <davideriksson@swedenmail.com>
2015-07-05 00:03:08 +0200

    rensat lite i youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -8,22 +8,19 @@ function getBrowser() {
 	test -n "$LYNX" && echo "lynx"; exit 1;
 	test -n "$ELINKS" && echo "elinks"; exit 1;
 }
-TIMES=1
+TIMES=0
 function youtube() {
 	local TIMES=$(( $TIMES + 1 ))
 	local BROWSER="$( getBrowser )"
 	local MAXTIMES="$2";
 	local YOUTUBEURL="$1"
-	local YOUTUBEDIVIDER='watch?v='
-	local YOUTUBEID=$( echo $YOUTUBEURL | sed 's/"$YOUTUBEDIVIDER"/ /g' | awk '{print "$2"}' )
+	test -z "$YOUTUBEURL" && exit;
 	local YOUTUBETITLE=$( $BROWSER "$YOUTUBEURL" -source | grep '"eow-title"' | awk -F'title="' '{print $2}' | awk -F'">' '{print $1}' )
-	local RECOMMENDEDVIDEOS=$( echo $( $BROWSER "$YOUTUBEURL" -dump | grep "$YOUTUBEDIVIDER" | awk -F'. ' '{print $3}' ) | tr ' ' '\n' > /tmp/youtube )
+	local RECOMMENDEDVIDEOS=$( echo $( $BROWSER "$YOUTUBEURL" -dump | grep 'watch?v=' | awk -F'. ' '{print $3}' ) | tr ' ' '\n' > /tmp/youtube )
 	local MAX=$( less /tmp/youtube | wc -l )
 	local RAND=$(( ( RANDOM % MAX )  + 1 ))
 	local RANDOMVIDEO=$( less '/tmp/youtube' | sed -n "$(echo $RAND)p" )
-	if [[ -z "$YOUTUBEURL" ]]; then
-		exit;
-	fi
+	test -z "$RANDOMVIDEO" && exit;
 	echo TITLE "$TIMES": "$YOUTUBETITLE" "$YOUTUBEURL"
 	if [[ $MAXTIMES != 0 && $MAXTIMES -eq $TIMES ]]; then
 		open "$YOUTUBEURL"
@@ -33,6 +30,6 @@ function youtube() {
 }
 
 #Default-values
-URL="${1:-www.youtube.com}"
-MAX_TIMES="${2:-1000}"
+readonly URL="${1:-www.youtube.com}"
+readonly MAX_TIMES="${2:-1000}"
 youtube "$URL" "$MAX_TIMES"
