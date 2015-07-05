15884be989f14d88859cd02f665010bab5ab771e
david eriksson <davideriksson@swedenmail.com>
2015-07-05 12:32:27 +0200

    småfixar på youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -10,18 +10,17 @@ function getBrowser() {
 }
 TIMES=0
 function youtube() {
-	local TIMES=$(( $TIMES + 1 ))
+	TIMES=$(( TIMES + 1 ))
 	local BROWSER="$( getBrowser )"
 	local MAXTIMES="$2";
 	local YOUTUBEURL="$1"
-	test -z "$YOUTUBEURL" && exit;
 	local YOUTUBETITLE=$( $BROWSER "$YOUTUBEURL" -source | grep '"eow-title"' | awk -F'title="' '{print $2}' | awk -F'">' '{print $1}' )
-	local RECOMMENDEDVIDEOS=$( echo $( $BROWSER "$YOUTUBEURL" -dump | grep 'watch?v=' | awk -F'. ' '{print $3}' ) | tr ' ' '\n' > /tmp/youtube )
+	$( echo $( $BROWSER "$YOUTUBEURL" -dump | grep 'watch?v=' | awk -F'. ' '{print $3}' ) | tr ' ' '\n' | grep -Ev 'android-app|ios-app' > /tmp/youtube )
 	local MAX=$( less /tmp/youtube | wc -l )
 	local RAND=$(( ( RANDOM % MAX )  + 1 ))
 	local RANDOMVIDEO=$( less '/tmp/youtube' | sed -n "$(echo $RAND)p" )
-	test -z "$RANDOMVIDEO" && exit;
-	echo TITLE "$TIMES": "$YOUTUBETITLE" "$YOUTUBEURL"
+	printf "%s TITLE: $TIMES: $YOUTUBETITLE $YOUTUBEURL\n" "$(tput setaf 2)"
+	tput sgr0
 	if [[ $MAXTIMES != 0 && $MAXTIMES -eq $TIMES ]]; then
 		open "$YOUTUBEURL"
 		exit;
